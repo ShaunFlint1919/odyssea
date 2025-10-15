235c6e17076103331e6819d9535d81aa4e0c3493
flintshaun26 <39033882-flintshaun26@users.noreply.replit.com>
2025-10-15 16:14:39 +0000

    Restored to 'ce356bdead9cf2a7c063896a0a19182a88ccdd0c'
    Replit-Restored-To: ce356bdead9cf2a7c063896a0a19182a88ccdd0c

--- odyssea/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function odyssea_enqueue_assets() {
-    // Charger les fichiers compilés de ton front
-    wp_enqueue_style('odyssea-style', get_template_directory_uri() . '/client/style.css');
-    wp_enqueue_script('odyssea-script', get_template_directory_uri() . '/client/main.js', array(), null, true);
-}
-add_action('wp_enqueue_scripts', 'odyssea_enqueue_assets');
